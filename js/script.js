f982124ffb1a0f823226dcbce7ab81f5339846cb
Richard Ng <richardkentng@gmail.com>
2022-03-12 20:05:45 -0800

    toggles display of nav contents on click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("sanity check");
+
+//on mobile devices, when the list icon is clicked, toggle display of navbar:
+const nav = document.body.querySelector("nav");
+nav.addEventListener("click", toggleDisplayNav);
+
+function toggleDisplayNav() {
+  nav.classList.toggle("display");
+}
