4effecde7749a4192b9d08e3ca0138c7f230c6c7
Richard Ng <richardkentng@gmail.com>
2022-03-13 18:18:18 -0700

    makes hamburger show list icon or x icon

--- js/script.js
+++ js/script.js
@@ -2,21 +2,34 @@ console.log("sanity check");
 
 const nav = document.body.querySelector("nav");
 
-// when list icon is clicked, toggle display class of nav (Observable on narrow window widths)
-const listIcon = nav.querySelector(".list-icon");
-listIcon.addEventListener("click", () => {
-  affectDisplayNav("toggle");
+// when hamburger (icon with 3 lines) is clicked, toggle display class of nav (Observable on narrow window widths)
+const hamburger = nav.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  navDisplayClass("toggle");
 });
 
 // when nav link is clicked, remove display class from nav (Observable on narrow window widths)
 const navLinks = nav.querySelectorAll(".nav-links a");
 navLinks.forEach((navLink) =>
   navLink.addEventListener("click", () => {
-    affectDisplayNav("remove");
+    navDisplayClass("remove");
   })
 );
 
 // functions
-function affectDisplayNav(classListMethod) {
+
+function navDisplayClass(classListMethod) {
   nav.classList[classListMethod]("display");
+  chooseHamburgerIcon(nav.classList.contains("display"));
+
+  //local functions
+  function chooseHamburgerIcon(navIsDisplayed) {
+    //clear hamburger's icon classes
+    ["bi-list", "bi-x"].forEach((biClass) =>
+      hamburger.classList.remove(biClass)
+    );
+    //add icon to hamburger
+    const biClass = navIsDisplayed ? "bi-x" : "bi-list";
+    hamburger.classList.add(biClass);
+  }
 }
